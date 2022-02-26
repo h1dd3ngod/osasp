8a7e656e1f74a077784b2f54dee530b21bdf1129
h1dd3ngod <nikbog03@gmail.com>
2022-02-27 02:06:15 +0300

    complete

--- lab1/find-string.bash
+++ lab1/find-string.bash
@@ -4,8 +4,11 @@ if [ "$#" -ne 2 ]
 then
     echo "wrong arguments" >&2
     echo "use scriptname <string> <dir>"
+elif [ ! -d "$2" ]
+then
+   echo "no such directory" >&2
+   echo "aborting" >&2
 else
-   find $2 -type f -exec grep -H "$1" {} \; | sort -M -n | ls -l
-  
-    
-   
+    find $2 -type f -exec grep -r --regexp "$1" {} 2>/dev/null -l  \; | xargs ls -al | sort -k 5 -n | awk '{print $5, $9}'
+                                                                                  #-Sr
+fi
